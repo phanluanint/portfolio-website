5d75e99b532882a29122bfbf6353b03ac67dfe5d
Luan Phan <phanluanint@gmail.com>
2020-01-08 21:05:56 +0700

    test

---
+++ next.config.js
@@ -0,0 +1,5 @@
+// next.config.js
+const withCss = require('@zeit/next-css')
+module.exports = withCss({
+  /* config options here */
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import {NextPage} from 'next'
+import '../styles/index.css'
+import Button from "../components/Button";
 
 const Home: NextPage<{ userAgent: string }> = ({userAgent}) => (
-    <h1>Hello world! - user agent: {userAgent}</h1>
+    <body className="bg-blue-500 p-12">
+        <div className="p-4 shadow rounded bg-white">
+            <h1 className="text-purple-500 leading-normal">Next.js</h1>
+            <p className="text-gray-500">with Tailwind CSS</p>
+            <Button onClick={(e) => alert('asd')}>
+                Child
+            </Button>
+        </div>
+    </body>
 );
 Home.getInitialProps = async ({req}) => {
     const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
