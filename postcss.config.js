c0dd513408e8acfbcb2f2e45048e68c3e3384eda
Luan Phan <phanluanint@gmail.com>
2020-02-25 09:45:56 +0700

    deploy to now

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,11 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project 
   content: [
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
     // etc.
   ],
+  whitelist: ['body'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
