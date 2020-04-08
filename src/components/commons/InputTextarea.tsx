ae078313d92097f294d6db41690044f3f8950225
Luan Phan <phanluanint@gmail.com>
2020-04-08 22:13:54 +0700

    more error reporting

--- src/components/commons/InputTextarea.tsx
+++ src/components/commons/InputTextarea.tsx
@@ -11,11 +11,15 @@ interface InputTextareaProps {
 const InputTextarea: React.FC<InputTextareaProps> = ({ name, value, label, rows, error, onChange }) => {
   return (
     <div className="relative my-8">
-      <label className="absolute top-0 left-0 text-gray-500 -mt-3 ml-3 px-3 bg-gray-1100" htmlFor={name}>
+      <label
+        className={`absolute top-0 left-0 text-gray-500 -mt-3 ml-3 px-3 bg-gray-1100 ${error && 'text-red-500'}`}
+        htmlFor={name}
+      >
         {label}
       </label>
       <textarea
-        className="w-full border border-gray-800 bg-transparent p-3 text-gray-400 outline-none hover:outline-none focus:border-gray-600 transition duration-300 ease-out"
+        className={`w-full border border-gray-800 bg-transparent p-3 text-gray-400 outline-none hover:outline-none focus:border-gray-600 transition duration-300 ease-out  ${error &&
+          'border-red-500'}`}
         name={name}
         id={name}
         value={value}
