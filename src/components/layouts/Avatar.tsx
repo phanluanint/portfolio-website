562a5e08a5bdd99ca48f44537506cb6e739afa46
Luan Phan <luan@wearebloom.com.au>
2025-05-24 14:05:29 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/layouts/Avatar.tsx
+++ src/components/layouts/Avatar.tsx
@@ -4,7 +4,7 @@ const Avatar: React.FC = () => {
   return (
     <div className="w-full h-56 flex items-center justify-center">
       <div className="w-40 h-40 p-2 bg-gray-500 rounded-full">
-        <img className="rounded-full" src="/images/avatar_small_fit.png" alt="Avatar" />
+        <img className="rounded-full" src="/images/1671976418537.jpg" alt="Avatar" />
       </div>
     </div>
   )
